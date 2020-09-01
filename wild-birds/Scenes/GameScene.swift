6b082c35a298965b97921b329d8c9e4462f489e8
rogerprz <rogerprz@github.com>
2020-08-31 21:46:44 -0700

    add pan gesuter with no limits

--- wild-birds/Scenes/GameScene.swift
+++ wild-birds/Scenes/GameScene.swift
@@ -15,6 +15,7 @@ class GameScene: SKScene {
     
     let gameCamera = GameCamera()
     var panRecognizer = UIPanGestureRecognizer()
+    var pinchRecognizer = UIPinchGestureRecognizer()
     
     override func didMove(to view: SKView) {
         setupLevel()
@@ -25,6 +26,9 @@ class GameScene: SKScene {
         guard let view = view else { return }
         panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(pan))
         view.addGestureRecognizer(panRecognizer)
+        
+        pinchRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(pinch))
+        view.addGestureRecognizer(pinchRecognizer)
     }
     
     func setupLevel(){
@@ -53,5 +57,30 @@ extension GameScene {
         gameCamera.position = CGPoint(x: gameCamera.position.x - translation.x, y: gameCamera.position.y + translation.y)
         
         sender.setTranslation(CGPoint.zero, in: view)
+    }
+    
+    @objc func pinch(sender: UIPinchGestureRecognizer){
+        guard let view = view else { return }
+        if sender.numberOfTouches == 2 {
+//            Give us position of gesture in view
+            let locationInView = sender.location(in: view)
+//            Give us defines location before we pinch
+            let location = convertPoint(fromView: locationInView)
+            if sender.state == .changed {
+                let convertedScale = 1/sender.scale
+                let newScale = gameCamera.yScale*convertedScale
+                gameCamera.setScale(newScale)
+//                Give position after scaling process
+                let locationAfterScale = convertPoint(fromView: locationInView)
+//                 Give positoin after scaling
+                let locationDelta = CGPoint(x: location.x - locationAfterScale.x, y: location.y - locationAfterScale.y)
+//
+                let newPosition = CGPoint(x: gameCamera.position.x + locationDelta.x, y: gameCamera.position.y + locationDelta.y)
+                gameCamera.position = newPosition
+                sender.scale = 1.0
+                gameCamera.setConstraints(with: self, and: mapNode.frame, to: nil)
+                
+            }
+        }
     }
 }
