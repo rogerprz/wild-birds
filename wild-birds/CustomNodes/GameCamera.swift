e0026eae6cb209ec6b94b08aa4f1f4c008df4f42
rogerprz <rogerprz@github.com>
2020-10-19 22:34:48 -0700

    fixed bug

--- wild-birds/CustomNodes/GameCamera.swift
+++ wild-birds/CustomNodes/GameCamera.swift
@@ -10,28 +10,26 @@ import SpriteKit
 
 class GameCamera: SKCameraNode {
 
-    func setConstraints(with scene: SKScene, and frame: CGRect, to node: SKNode?){
-        let scaledSize = CGSize(width:scene.size.width * xScale, height: scene.size.height * yScale)
+    func setConstraints(with scene: SKScene, and frame: CGRect, to node: SKNode?) {
+        let scaledSize = CGSize(width: scene.size.width * xScale, height: scene.size.height * yScale)
         let boardContentRect = frame
         
-        let xInset = min((scaledSize.width/2), boardContentRect.width/2)
-        let yInset = min((scaledSize.width/2), boardContentRect.height/2)
+        let xInset = min(scaledSize.width / 2, boardContentRect.width / 2)
+        let yInset = min(scaledSize.height / 2, boardContentRect.height / 2)
         let insetContentRect = boardContentRect.insetBy(dx: xInset, dy: yInset)
         
         let xRange = SKRange(lowerLimit: insetContentRect.minX, upperLimit: insetContentRect.maxX)
         let yRange = SKRange(lowerLimit: insetContentRect.minY, upperLimit: insetContentRect.maxY)
-        let levelEdgeConstraints = SKConstraint.positionX(xRange, y: yRange)
+        let levelEdgeConstraint = SKConstraint.positionX(xRange, y: yRange)
         
         if let node = node {
             let zeroRange = SKRange(constantValue: 0.0)
             let positionConstraint = SKConstraint.distance(zeroRange, to: node)
-            constraints = [positionConstraint, levelEdgeConstraints]
+            constraints = [positionConstraint, levelEdgeConstraint]
         } else {
-            constraints = [levelEdgeConstraints]
+            constraints = [levelEdgeConstraint]
         }
         
-        
-        
-        constraints = [levelEdgeConstraints]
+        constraints = [levelEdgeConstraint]
     }
 }
