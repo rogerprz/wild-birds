06ebfd4f2a1574792c9e4f6864e5021f48a7c24f
rogerprz <rogerprz@github.com>
2020-10-20 22:24:53 -0700

    launch and hit blocks

---
+++ wild-birds/CustomNodes/Block.swift
@@ -0,0 +1,51 @@
+//
+//  Block.swift
+//  wild-birds
+//
+//  Created by Roger Perez on 10/20/20.
+//  Copyright © 2020 CodeRoger. All rights reserved.
+//
+
+import SpriteKit
+
+enum BlockType: String {
+    case wood, stone, glass
+}
+
+class Block: SKSpriteNode {
+    
+    let type: BlockType
+    var health: Int
+    let damageThreshold: Int
+    
+    init(type: BlockType) {
+        self.type = type
+        switch type {
+        case .wood:
+            health = 200
+        case .stone:
+            health = 500
+        case .glass:
+            health = 50
+        }
+        damageThreshold = health/2
+        super.init(texture: nil, color: UIColor.clear, size: CGSize.zero)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func createPhysicsBody() {
+        physicsBody = SKPhysicsBody(rectangleOf: size)
+        physicsBody?.isDynamic = true
+        physicsBody?.categoryBitMask = PhysicsCategory.block
+        physicsBody?.contactTestBitMask = PhysicsCategory.all
+        physicsBody?.collisionBitMask = PhysicsCategory.all
+    }
+    
+    
+    
+    
+    
+}
